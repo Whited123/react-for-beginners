328c3de8d8e7e55670b9816b22019245fd064070
kim dong young <dudtodtls24@gmail.com>
2023-05-19 21:56:51 +0900

    리액트 js 5일차 6.4
    이펙트 정복완료... 이제 "무비 배포어"다.

--- react-for-beginners2/src/App.js
+++ react-for-beginners2/src/App.js
@@ -2,30 +2,26 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
+function Hello() {
+  function byFn() {
+    console.log("간다");
+  }
+  function hiFn() {
+    console.log("내가 여기왔다.");
+    return byFn;
+  }
+  useEffect(hiFn, []);
+
+  return <h1>하이</h1>;
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log("난 항상 실행된다...");
-  useEffect(() => {
-    console.log("난 한번만 실행되지..");
-  }, []);
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) {
-      console.log("난", keyword, "를 검색하고 싶어");
-    }
-  }, [keyword]);
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="설치 히어..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>날 눌러</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Showing"}</button>
     </div>
   );
 }
