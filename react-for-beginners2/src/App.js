eec29db731511bd81eb39b5d38d81d7b844ee58b
kim dong young <dudtodtls24@gmail.com>
2023-05-19 20:48:44 +0900

    리액트 즈스 5일차 6.1
    effect는 중복실행을 막기 위해 ()안에 첫번째에 펑션을 넣고, 두번째는 [] 를 넣는데, 이 [] 는 다음강의에서 배운다

--- react-for-beginners2/src/App.js
+++ react-for-beginners2/src/App.js
@@ -1,10 +1,15 @@
 import Button from "./Button";
 import styles from "./App.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
+  console.log("난 항상 실행된다...");
+  const iOnlyOnceUse = () => {
+    console.log("난 단 한번 실행되지...");
+  };
+  useEffect(iOnlyOnceUse, []);
   return (
     <div>
       <h1>{counter}</h1>
