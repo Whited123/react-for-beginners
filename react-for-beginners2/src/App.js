5494f06fe2166da50a91ddef40ffd97a7d8dd369
kim dong young <dudtodtls24@gmail.com>
2023-05-19 21:15:41 +0900

    리액트 즈스 5일차 6.2
    이펙트의 []에는 엄청난 마법이 들어있었다... 바로 어레이 안에 들어있는 요소가 실행될때마다 펑션을 보여주는것... 엄청나다.

--- react-for-beginners2/src/App.js
+++ react-for-beginners2/src/App.js
@@ -4,14 +4,26 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   console.log("난 항상 실행된다...");
-  const iOnlyOnceUse = () => {
-    console.log("난 단 한번 실행되지...");
-  };
-  useEffect(iOnlyOnceUse, []);
+  useEffect(() => {
+    console.log("난 한번만 실행되지..");
+  }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("난", keyword, "를 검색하고 싶어");
+    }
+  }, [keyword]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="설치 히어..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>날 눌러</button>
     </div>
