ee037e867ce1d727a46efd97462937b3c3dfa439
kim dong young <dudtodtls24@gmail.com>
2023-05-20 23:38:39 +0900

    리액트 6일차 7.0
    투두리스트 만들기... 바닐라의 악몽이 떠오른다... 지금까지의 복습

--- react-for-beginners2/src/App.js
+++ react-for-beginners2/src/App.js
@@ -2,26 +2,33 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
-function Hello() {
-  function byFn() {
-    console.log("간다");
-  }
-  function hiFn() {
-    console.log("내가 여기왔다.");
-    return byFn;
-  }
-  useEffect(hiFn, []);
-
-  return <h1>하이</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => {
+    setToDo(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDo("");
+    setToDos((currentArray) => [toDo, ...currentArray]);
+  };
+  console.log(toDos);
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Showing"}</button>
+      <h1>해야 할 일({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="니가 할 일을 적으세요..."
+        />
+        <button>할 일 더하기</button>
+      </form>
     </div>
   );
 }
